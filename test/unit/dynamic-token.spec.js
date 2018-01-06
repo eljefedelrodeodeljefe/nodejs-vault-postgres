dd81a6c8da2479211afcd13b593898e09f302b56
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2018-01-07 00:45:54 +0100

    externalise pool

--- test/unit/dynamic-token.spec.js
+++ test/unit/dynamic-token.spec.js
@@ -1,7 +1,7 @@
 const test = require('tape')
-const { DynamicPool } = require('../../lib/helpers/dbs/pg/dynamic-pool')
+const DynamicPool = require('../../lib/helpers/dbs/pg/dynamic-pool/pool')
 
-test('Dymaic Token: can instantiate token retrieval', function (t) {
+test('Dynamic Token: can instantiate token retrieval', (t) => {
   t.plan(2)
 
   t.doesNotThrow(() => {
