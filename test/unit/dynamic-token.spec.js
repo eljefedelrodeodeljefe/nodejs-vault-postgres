cf3075e35856061ecd8f429328b2c93c95f0b5e1
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2018-01-06 21:58:31 +0100

    add dynamic pool

--- test/unit/dynamic-token.spec.js
+++ test/unit/dynamic-token.spec.js
@@ -1,7 +1,13 @@
 const test = require('tape')
+const { DynamicPool } = require('../../lib/helpers/dbs/pg/dynamic-pool')
 
 test('Dymaic Token: can instantiate token retrieval', function (t) {
   t.plan(2)
-  t.fail()
+
+  t.doesNotThrow(() => {
+    const pool = new DynamicPool()
+    t.ok(pool, 'Pool is truthy existing.')
+  }, 'Pool does not throw upon instantiation.')
+
   t.end()
 })
