076a88735a54c00d0a31e6e4876acc09680add2d
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2018-01-07 23:36:40 +0100

    fix test run

--- test/unit/dynamic-token.spec.js
+++ test/unit/dynamic-token.spec.js
@@ -1,4 +1,5 @@
 const test = require('tape')
+const sinon = require('sinon')
 const DynamicPool = require('../../lib/helpers/dbs/pg/dynamic-pool/pool')
 
 test('Dynamic Token: can instantiate token retrieval', (t) => {
@@ -46,20 +47,22 @@ test('Dynamic Token: can provide singleton that is of instance DynamicPool', (t)
 })
 
 test('Dynamic Token: will be initialised w/o and then get credentials', (t) => {
-  t.plan(5)
-
-  const pool = new DynamicPool()
-
-  t.equals(pool.credentials, null, 'credentials are not initially')
-
-  pool.once('credentials', (instance, credentials) => {
+  t.plan(6)
+  const callback = sinon.spy((instance, credentials) => {
     t.ok(credentials, 'sucessfully fetched credentials')
     t.ok(Number.isFinite(credentials.lease_duration), 'got a lease and it\'s a Number')
     t.ok(credentials.data.password && typeof credentials.data.password === 'string', 'received password as string')
     t.ok(credentials.data.username && typeof credentials.data.username === 'string', 'received username as string')
 
     pool.destroy()
+
+    t.ok(callback.calledOnce)
   })
+
+  const pool = new DynamicPool()
+  t.equals(pool.credentials, null, 'credentials are not initially')
+
+  pool.on('credentials', callback)
 })
 
 test('Dynamic Token: can destroy pool instance', (t) => {
@@ -78,5 +81,7 @@ test('Dynamic Token: can destroy pool instance', (t) => {
     t.equals(pool.destroyed, true)
     t.equals(pool.isDirty, false)
     t.equals(pool.credentials, null)
+
+    t.end()
   })
 })
